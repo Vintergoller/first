0a746234a89287dfa728c037b3ae6ea526d3c146
Vintergoller <Pererva_nik@mail.ru>
2024-09-12 10:43:17 +0300

    stickyHeadAndButtonUpdate

--- SmartHouseProject/js/stickyHead.js
+++ SmartHouseProject/js/stickyHead.js
@@ -1,20 +1,20 @@
 let header = document.querySelector('.js-header'),
 headerH = document.querySelector('.js-header').clientHeight;
 
-let headertext = document.querySelector('._textUnlock');
+
 
 
 document.onscroll = function(){
     let scroll = window.scrollY;
     console.log(scroll);
-    if(scroll>headerH + 120){
+    if(scroll>headerH + 100){
         header.classList.add('fixed');
-        headertext.classList.add('mainTextUnfixed');
+      
         
     }
     else{
         header.classList.remove('fixed');
-        headertext.classList.remove('mainTextUnfixed');
-        document.body.removeAttribute('style');
+  
+      
     }
 }
